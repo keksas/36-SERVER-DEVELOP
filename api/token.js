e92e95719fce96ead8b424bcb0058222ca764a6b
keksas <k.rimantas@gmail.com>
2022-04-12 15:06:45 +0300

    .gitignor edit

--- api/token.js
+++ api/token.js
@@ -1,3 +1,8 @@
+import { file } from "../lib/file.js";
+import { IsValid } from "../lib/IsValid.js";
+import { utils } from "../lib/utils.js";
+import config from "../config.js";
+
 const handler = {};
 
 handler.token = (data, callback) => {
@@ -12,11 +17,97 @@ handler.token = (data, callback) => {
 
 handler._method = {};
 
-handler._method.post = (data, callback) => {
-  return callback(200, {
-    action: "POST",
-    msg: "Vartotojui isduotas sesijos TOKEN",
-  });
+handler._method.post = async (data, callback) => {
+  const user = data.payload;
+  const { email, password } = user;
+  const requiredKeys = 2;
+
+  const [emailErr, emailMsg] = IsValid.email(email);
+  if (emailErr) {
+    return callback(400, {
+      status: "Error",
+      msg: emailMsg,
+    });
+  }
+
+  const [passwordErr, passwordMsg] = IsValid.password(password);
+  if (passwordErr) {
+    return callback(400, {
+      status: "Error",
+      msg: passwordMsg,
+    });
+  }
+
+  if (requiredKeys !== Object.keys(user).length) {
+    return callback(400, {
+      status: "Error",
+      msg: "Netinkama objekto struktura (turi buti tik: email, password)",
+    });
+  }
+
+  const [readErr, readMsg] = await file.read("accounts", email + ".json");
+  if (readErr) {
+    return callback(400, {
+      status: "Error",
+      msg: "Klaida bandant prisijungti: nesutampa email arba slaptazodis",
+    });
+  }
+
+  const userObj = utils.parseJSONtoObject(readMsg);
+  if (!userObj) {
+    return callback(500, {
+      status: "Error",
+      msg: "Klaida bandant nuskaityti vartotojo duomenis",
+    });
+  }
+
+  const hashedPassword = utils.hash(password);
+  if (hashedPassword !== userObj.password) {
+    return callback(400, {
+      status: "Error",
+      msg: "Klaida bandant prisijungti: nesutampa email arba slaptazodis",
+    });
+  }
+
+  const tokenID = utils.randomString(config.sessionTokenLength);
+  const token = {
+    expire: Date.now() + config.cookiesMaxAge * 1000,
+    email: email,
+    browser: "chrome",
+  };
+
+  const [createErr] = await file.create("token", tokenID + ".json", token);
+  if (createErr) {
+    return callback(500, {
+      status: "Error",
+      msg: "Klaida bandant isduoti sesijos Token",
+    });
+  }
+
+  const cookies = [
+    "login-token=" + tokenID,
+    "path=/",
+    "domain=localhost",
+    "max-age=" + config.cookiesMaxAge,
+    "expires=Sun, 16 Jul 3567 06:23:41 GMT",
+    // 'Secure',
+    "SameSite=Lax",
+    "HttpOnly",
+  ];
+
+  return callback(
+    200,
+    {
+      status: "Success",
+      msg: {
+        id: tokenID,
+        ...token,
+      },
+    },
+    {
+      "Set-Cookie": cookies.join("; "),
+    }
+  );
 };
 
 handler._method.get = (data, callback) => {
