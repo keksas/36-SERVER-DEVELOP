96d32ac2efa7a3f8c1894ce9df55161aff66e0ee
keksas <k.rimantas@gmail.com>
2022-04-20 18:15:11 +0300

    Server clean up initial function logic

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-import { server } from './lib/server.js';
+import { server } from "./lib/server.js";
 
 const app = {};
 
 app.init = () => {
-    // pasiruosti pradinius folder'ius
+  // pasiruosti pradinius folder'ius
 
-    // pasiruosti pradinius failus
+  // pasiruosti pradinius failus
 
-    // prisijungimas prie DB (duomenu baze)
+  // prisijungimas prie DB (duomenu baze)
 
-    // uzkurti pati serveri (musu programa)
-    server.init();
+  // uzkurti pati serveri (musu programa)
+  server.init();
 
-    // reguliariu procesu paleidimas:
-    // - istrinti senus/nebereikalingus failus
-    // - maziau naudojamu failu archivavimas
-    // - atsinaujinti informacija per/is API
-}
+  // reguliariu procesu paleidimas:
+  // - istrinti senus/nebereikalingus failus
+  server.cleanUp();
+  setInterval(server.cleanUp, 60 * 1000);
+  // - maziau naudojamu failu archivavimas
+  // - atsinaujinti informacija per/is API
+};
 
 app.init();
 
